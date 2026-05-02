1b25f51d381a6f2a26f1a1b2350d88e75e3944f9
dasafo81 <damianporter1@gmail.com>
2026-05-02 09:06:56 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App.jsx';
+import { ScreenLogin } from './components/ScreenLogin.jsx';
+import { loadSession } from './lib/auth.js';
+
+function Root() {
+  var initial = loadSession();
+  var ss = useState(!!initial); var loggedIn = ss[0]; var setLoggedIn = ss[1];
+
+  if (!loggedIn) {
+    return React.createElement(ScreenLogin, {
+      onLogin: function() { setLoggedIn(true); }
+    });
+  }
+
+  return React.createElement(App, {
+    onLogout: function() { setLoggedIn(false); }
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('app-mount')).render(
-  <App />
+  React.createElement(Root)
 );
